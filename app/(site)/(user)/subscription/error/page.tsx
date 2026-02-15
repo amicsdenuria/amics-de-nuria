838878177b6f0225bd436c411ed815132354abee
munergenis <munergenis@gmail.com>
2026-02-15 08:51:09 +0100

    feat: new error page

--- app/(site)/(user)/subscription/error/page.tsx
+++ app/(site)/(user)/subscription/error/page.tsx
@@ -1,15 +1,26 @@
 import { Button } from '@/components/ui/button';
-import PageContainer from '@/components/ui/page-container';
 import Link from 'next/link';
+import PageContainer from '@/components/ui/page-container';
 
 const ErrorPage = () => {
   return (
     <main className="flex-1 flex">
       <PageContainer className="py-4 flex-1 flex">
-        <div className="flex flex-col items-stretch md:items-start gap-8">
-          <div className="text-3xl font-bold max-w-3xl text-pretty">
-            Error al intentar crear la subscripció. Intenta-ho més tard o
-            posa&apos;t en contacte amb l&apos;organització.
+        <div className="flex flex-col items-stretch md:items-start py-8 gap-8">
+          <div>
+            <h1 className="text-3xl font-bold max-w-3xl text-pretty">
+              Alguna cosa ha fallat.
+            </h1>
+            <span>
+              Intenta accedir a{' '}
+              <Link
+                className="underline text-accent-foreground hover:text-muted-foreground"
+                href={'/subscription'}
+              >
+                Subscripció
+              </Link>{' '}
+              o posa&apos;t en contacte amb l&apos;organització.
+            </span>
           </div>
           <Button asChild>
             <Link
