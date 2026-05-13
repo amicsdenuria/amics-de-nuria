b8144e51298ed66317f8399cf48f055c45b2dfbd
munergenis <munergenis@gmail.com>
2026-05-13 11:43:23 +0200

    chore: hide llar amadeu subpage

--- app/(site)/(public)/santuari/llar-amadeu/page.tsx
+++ app/(site)/(public)/santuari/llar-amadeu/page.tsx
@@ -1,10 +1,15 @@
 import PrimaryPageHero from '../../components/PrimaryPageHero';
 import TextBlock from '../../components/TextBlock';
+import { notFound } from 'next/navigation';
 import { santuariContent } from '@/content/santuari/santuariPage';
 
 const LlarAmadeuPage = () => {
+  // TODO pendent d'activar (llar amadeu) ACTIVAR LA DE BAIX
+  const isActive = false;
   const { llarAmadeu } = santuariContent.pages;
-  return (
+  return !isActive ? (
+    notFound()
+  ) : (
     <>
       <PrimaryPageHero
         title={llarAmadeu.title}
@@ -31,5 +36,36 @@ const LlarAmadeuPage = () => {
     </>
   );
 };
+
+// TODO pendent d'activar (llar amadeu)
+// const LlarAmadeuPage = () => {
+//   const { llarAmadeu } = santuariContent.pages;
+//   return (
+//     <>
+//       <PrimaryPageHero
+//         title={llarAmadeu.title}
+//         subtitle={llarAmadeu.intro}
+//         img={{
+//           src: '/santuari/vall-nuria-2.webp',
+//           alt: 'Vall de Núria',
+//           className: 'object-center',
+//         }}
+//       />
+
+//       <section className="py-16 md:py-24">
+//         <div className="mx-auto max-w-3xl px-6">
+//           <div className="space-y-12">
+//             {llarAmadeu.blocks.map((block, i) => (
+//               <TextBlock
+//                 key={i}
+//                 block={block}
+//               />
+//             ))}
+//           </div>
+//         </div>
+//       </section>
+//     </>
+//   );
+// };
 
 export default LlarAmadeuPage;
--- content/santuari/santuariPage.ts
+++ content/santuari/santuariPage.ts
@@ -11,7 +11,8 @@ const santuariNavItems = [
   },
   { label: "L'Ermita de Sant Gil", href: '/santuari/sant-gil' },
   { label: 'El Via Crucis', href: '/santuari/via-crucis' },
-  { label: 'La Llar Amadeu', href: '/santuari/llar-amadeu' },
+  // TODO: Pendent d'activar (llar amadeu)
+  // { label: 'La Llar Amadeu', href: '/santuari/llar-amadeu' },
   { label: 'Com arribar-hi', href: '/santuari/com-arribar' },
 ];
 
@@ -71,11 +72,12 @@ export const santuariContent: SantuariContent = {
           description: santuariSubpages.viaCrucis.intro,
           href: '/santuari/via-crucis',
         },
-        {
-          title: 'La Llar Amadeu',
-          description: santuariSubpages.llarAmadeu.intro,
-          href: '/santuari/llar-amadeu',
-        },
+        // TODO: Pendent d'activar (llar amadeu)
+        // {
+        //   title: 'La Llar Amadeu',
+        //   description: santuariSubpages.llarAmadeu.intro,
+        //   href: '/santuari/llar-amadeu',
+        // },
         {
           title: 'Com arribar-hi',
           description: santuariSubpages.comArribar.intro,
