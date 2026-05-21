e72cd6a9b304dd1e1d51c04184617db9a3215360
munergenis <munergenis@gmail.com>
2026-05-21 12:50:21 +0200

    feat: Route/Stage Hero Title full width

--- app/(site)/(public)/rutes-itineraris/components/RouteHero.tsx
+++ app/(site)/(public)/rutes-itineraris/components/RouteHero.tsx
@@ -62,7 +62,7 @@ const RouteHero = ({
       <div className="absolute inset-0 bg-linear-to-t from-background via-background/85 md:via-background/70 to-background/20 dark:to-background/20" />
 
       <PageContainer className="relative h-full flex flex-col justify-end">
-        <div className="max-w-5xl">
+        <div className="w-full">
           {/* BACK BUTTON */}
           {type === 'route' && (
             <Button
